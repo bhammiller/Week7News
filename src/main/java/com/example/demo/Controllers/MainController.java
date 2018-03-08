c5f6a03bcb7259c82933a56429d17d4543404323
Brandon Hammiller <bdayton1985@gmail.com>
2018-03-08 14:12:54 -0500

    It now shows the top news stories.

--- src/main/java/com/example/demo/Controllers/MainController.java
+++ src/main/java/com/example/demo/Controllers/MainController.java
@@ -1,9 +1,19 @@
 package com.example.demo.Controllers;
 
+import com.example.demo.Models.AppRole;
+import com.example.demo.Models.AppUser;
 import com.example.demo.Repositories.AppRoleRepository;
 import com.example.demo.Repositories.AppUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class MainController {
@@ -13,4 +23,46 @@ public class MainController {
 
     @Autowired
     AppRoleRepository appRoleRepository;
+
+    @Autowired
+    NewsService newsService;
+
+    // METHODS //////////////
+    // Homepage
+    @RequestMapping("/")
+    public String goToHome(Model model){
+        model.addAttribute("newslist",newsService.findTopHeadlines());
+        return "headlineslist";
+    }
+
+    // Login and Registraion
+    @RequestMapping("/login")
+    public String login() {
+        return "loginpage";
+    }
+
+    // Security and User Methods
+
+    @GetMapping("/register")
+    public String registerUser(Model model) {
+        model.addAttribute("newUser", new AppUser());
+        return "registrationpage";
+    }
+
+    @PostMapping("/register")
+    public String addNewUser(@Valid @ModelAttribute("newUser") AppUser newUser, BindingResult result, Model model) {
+
+        if (result.hasErrors()) {
+            System.out.println(result.toString());
+            return "registrationpage";
+        } else {
+
+            model.addAttribute(newUser.getAppUsername() + " created");
+            appUserRepository.save(newUser);
+            AppRole r = appRoleRepository.findAppRoleByRoleName("USER");
+            newUser.addRole(r);
+            appUserRepository.save(newUser);
+            return "redirect:/login";
+        }
+    }
 }
--- src/main/java/com/example/demo/Controllers/NewsService.java
+++ src/main/java/com/example/demo/Controllers/NewsService.java
@@ -1,7 +1,19 @@
 package com.example.demo.Controllers;
 
+import com.example.demo.Models.Article;
+import com.example.demo.Models.Headlines;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class NewsService {
+
+    public Iterable<Article> findTopHeadlines(){
+        RestTemplate restTemplate = new RestTemplate();
+        Headlines headlines=restTemplate.getForObject("https://newsapi.org/v2/top-headlines?country=us&apiKey=1f6e68f888ff45538990a57fa0e356b0",
+                Headlines.class);
+        System.out.println(headlines.toString());
+        System.out.println();
+        return headlines.getArticles();
+    }
 }
